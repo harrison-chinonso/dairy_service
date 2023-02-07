6edcc49345b654dc17f622bccbfc65a7d536e71b
chinonsoharrison <chinonsoikwueto@gmail.com>
2023-02-07 09:30:44 +0100

    **update commit** 1. user registration 2. user login and authentication

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"dairy_service/controller"
 	"dairy_service/database"
 	"dairy_service/model"
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log"
 )
@@ -10,6 +13,7 @@ import (
 func main() {
 	loadEnv()
 	loadDatabase()
+	serveApplication()
 }
 
 func loadDatabase() {
@@ -23,4 +27,15 @@ func loadEnv() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
+
+func serveApplication() {
+	router := gin.Default()
+
+	publicRoutes := router.Group("/auth")
+	publicRoutes.POST("/register", controller.Register)
+	publicRoutes.POST("/login", controller.Login)
+
+	router.Run(":8000")
+	fmt.Println("Server running on port 8000")
 }
